b7af03df7938446b63743b4aa4324b526eea9cc6
huweicheng <yoouwei@gmail.com>
2022-03-19 10:03:33 -0600

    add empty class for add schedule page

---
+++ app/src/main/java/com/example/weeklyschedule/AddScheduleActivity.java
@@ -0,0 +1,19 @@
+package com.example.weeklyschedule;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddScheduleActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_schedule);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
