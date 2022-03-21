9f653401f01f71a8adee58d4ddd9dd744e976ce8
huweicheng <yoouwei@gmail.com>
2022-03-21 15:28:12 -0600

    add empty class for select schedule page

---
+++ app/src/main/java/com/example/weeklyschedule/ScheduleListActivity.java
@@ -0,0 +1,19 @@
+package com.example.weeklyschedule;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScheduleListActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_list);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
