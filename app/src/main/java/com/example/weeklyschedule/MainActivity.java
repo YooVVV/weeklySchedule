f2c33205b882a17e06065aa2c156fb492eb28bd8
huweicheng <yoouwei@gmail.com>
2022-03-21 15:28:56 -0600

    US 1 is implemented

--- app/src/main/java/com/example/weeklyschedule/MainActivity.java
+++ app/src/main/java/com/example/weeklyschedule/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.weeklyschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button add_schedule;
+    private Button select_schedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add_schedule = findViewById(R.id.main_add_schdule_btn);
+        add_schedule.setOnClickListener(this);
+        select_schedule = findViewById(R.id.main_select_schdule_btn);
+        select_schedule.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_add_schdule_btn:
+                startActivity(new Intent(this, AddScheduleActivity.class));
+                break;
+            case R.id.main_select_schdule_btn:
+                startActivity(new Intent(this, ScheduleListActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
