23e59c4b196e2f728026fe9ee3ca59b8bb528218
huweicheng <yoouwei@gmail.com>
2022-03-22 14:58:23 -0600

    implement back button in schedule list page

--- app/src/main/java/com/example/weeklyschedule/ScheduleListActivity.java
+++ app/src/main/java/com/example/weeklyschedule/ScheduleListActivity.java
@@ -2,18 +2,24 @@ package com.example.weeklyschedule;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScheduleListActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_list);
+        back = findViewById(R.id.schedule_list_back_btn);
+        back.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view.getId() == R.id.schedule_list_back_btn) {
+            this.finish();
+        }
     }
 }
